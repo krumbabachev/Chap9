05392eef0f79f12f34d8ecee9e64495fe7ad811a
Krum Babachev <44364634+krumbabachev@users.noreply.github.com>
2018-12-02 03:22:58 +0200

    Add files via upload

---
+++ Chap9/Chap9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chap9
+{
+    class Program
+    {
+
+        static void ReturnName(string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            ReturnName(Console.ReadLine());
+        }
+    }
+}
